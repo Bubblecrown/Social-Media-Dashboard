76b3df5fed5efdea38c597add63b5a4e8cbbaedd
Bubblecrown <suchanya.namwong1@gmail.com>
2022-08-12 02:19:38 +0700

    finish NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function NavBar() {
+  const [enabled, setEnabled] = useState(false);
   return (
     <>
-      <main className=" grid px-32 py-8">
-        <section className="flex flex-wrap flex-col justify-center items-start ">
-          <h1 className="text-2xl font-bold">Social Media Dashboard</h1>
-          <p className="text-sm">Total Followers: 23,004</p>
+      <nav className="px-52 py-8 flex justify-between">
+        <section className="flex flex-wrap flex-col justify-center items-start">
+          <h1 className="text-2xl font-bold text-[color:var(--VeryDarkBlue)]">
+            Social Media Dashboard
+          </h1>
+          <p className="text-[12.5px] font-semibold text-[color:var(--DarkGrayishBlue)]">
+            Total Followers: 23,004
+          </p>
         </section>
-      </main>
+        <section className="flex flex-rows gap-3 items-center">
+          <p className="text-sm font-semibold text-[color:var(--DarkGrayishBlue)]">
+            Dark Mode
+          </p>
+          <span className="relative items-center cursor-pointer">
+            {/* sr-only ซ่อนเอาไว้ในเพจปกติเพื่อแสดงในโปรแกรมการอ่านหหน้าจอเท่านั้น เช่น ฟอร์มที่ไม่มี label เขียนไว้จะเป็นปัญหาสำหรับผู้มีปัญหาสายตาที่ใช้โปรแกรมการอ่านหน้าจอ แต่เราก็ต้องการสไตล์แบบไม่มี label เช่นกัน เราเลยใช้ sr-only เพื่อซ่อนมันในปกติ แต่มันก็จะแสดงหากมีใครใช้โปรแกรมการอ่านหน้าจอ*/}
+            {/* ในกรณีนี้เราใช้ sr-only ในการซ่อนปุ่ม checkbox หรือจะใช้อย่างอื่นเช่น appearance none ก็ได้ */}
+            <input
+              type="checkbox"
+              // ใช peer เพื่อสังเกต action ตรงนี้
+              className="sr-only peer"
+              checked={enabled}
+              readOnly
+            />
+            <div
+              onClick={() => {
+                setEnabled(!enabled);
+              }}
+              // peer สังเกตพฤติกรรมของการเช็คเพื่อทำให้บางสิ่งเกิดขึ้นกับอย่างอื่น
+              // และมากระทำกับอันนี้
+              className="w-12 h-6 bg-gray-200 rounded-full peer  peer-checked:after:translate-x-[22px] after:content-[''] after:absolute after:top-0.5 after:left-[3px] after:bg-white  after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-gradient-to-l from-[color:var(--LimeGreen)] to-[color:var(--Twitter)]"
+            ></div>
+          </span>
+        </section>
+      </nav>
     </>
   );
 }
