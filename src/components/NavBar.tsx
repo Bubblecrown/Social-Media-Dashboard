d718d68bffef0df35e55659c1e9ab865d07c2d45
Bubblecrown <suchanya.namwong1@gmail.com>
2022-08-11 18:56:18 +0700

    setting color root

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <>
+      <main className=" grid px-32 py-8">
+        <section className="flex flex-wrap flex-col justify-center items-start ">
+          <h1 className="text-2xl font-bold">Social Media Dashboard</h1>
+          <p className="text-sm">Total Followers: 23,004</p>
+        </section>
+      </main>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        fluid: "repeat(auto-fit, minmax(20rem, 1fr))",
+      }
+    },
+    fontFamily: {
+      inter: "Inter",
+    },
   },
   plugins: [],
-}
+};
