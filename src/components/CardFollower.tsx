60752223332f6b0742c77d3010f371376272df89
Bubblecrown <suchanya.namwong1@gmail.com>
2022-08-13 22:41:10 +0700

    condition className Today rate growth

--- src/components/CardFollower.tsx
+++ src/components/CardFollower.tsx
@@ -10,7 +10,7 @@ export default function CardFollower(props: CardFollowerProps) {
     <>
       <section className={classProps}>
         <article className="flex flex-row gap-2 items-center">
-          <img src={props.image} alt="" className="w-5 h-auto"/>
+          <img src={props.image} alt="" className="w-5 h-auto" />
           <p className="text-[color:var(--DarkGrayishBlue)] ">
             {props.account}
           </p>
@@ -19,13 +19,19 @@ export default function CardFollower(props: CardFollowerProps) {
           <h1 className="text-[color::var(--VeryDarkBlue)] text-5xl font-bold">
             {props.amount}
           </h1>
-          <h3 className="mt-1 text-[color:var(--DarkGrayishBlue)] tracking-widest uppercase">
+          <h3 className="mt-2 text-[color:var(--DarkGrayishBlue)] tracking-[6px] uppercase text-[12px]">
             followers
           </h3>
         </article>
-        <article className="flex items-center gap-1 text-[color:var(--LimeGreen)] font-bold text-sm">
+        <article className="flex items-center gap-1 font-bold text-sm">
           <img src={props.arrow} alt="" />
-          <p className="">
+          <p
+            className={
+              props.arrow === "/static/media/icon-up.09cfa95e6e89e162c5c150356e6de276.svg"
+                ? "text-[color:var(--LimeGreen)]"
+                : "text-[color:var(--BrightRed)]"
+            }
+          >
             {props.today} Today
           </p>
         </article>
